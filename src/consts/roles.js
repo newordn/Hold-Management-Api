d326af6b5498c1b150cc305f9ccc61862527f9db
Neword <newordn@gmail.com>
2020-10-17 09:14:31 +0100

    Update roles.js

--- src/consts/roles.js
+++ src/consts/roles.js
@@ -1,7 +1,8 @@
 export const ROLES = {
-  admin: "Administrateur",
-  soutier: "Soutier",
-  donateur: "Donateur",
-  responsable: "Responsable de soute",
-  supervisuer: "Superviseur"
-}
+  administrateur: "administrateur",
+  soutier: "soutier",
+  donateur: "donateur",
+  responsable: "responsable",
+  superviseur: "superviseur",
+  emetteur: "emetteur",
+};
