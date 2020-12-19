b12e1dafb473c0808348f21ed73f0540f27ec879
Neword <newordn@gmail.com>
2020-12-19 14:12:38 +0100

    updated notification authToken

--- src/helpers/notification.js
+++ src/helpers/notification.js
@@ -1,5 +1,5 @@
 const accountSid = "AC25511275cb52991c4a3ec10e5ca96424"
-const authToken = "170aaa1fbde3c8ebcca29e2052aaba27"
+const authToken = "1e571a1c9881a2a53b1f9b2cf951dd7d"
 const my_number = "+16592342415"
 const client = require('twilio')(accountSid, authToken);
 const sendSms = async (number, message)=>{
