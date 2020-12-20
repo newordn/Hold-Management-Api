0926cd4c2cddbb02d746d1f4290660dd22674942
Neword <newordn@gmail.com>
2020-12-20 14:54:57 +0100

    old commit

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -267,13 +267,13 @@ const consumedBon = async (parent, args, context, info) => {
         ),
         user: { connect: { id: user } }
       });
-      const getUser = await context.prisma.user({ id: user });
       return { message: " Bon consommé avec succès", status };
     }
     console.log(
       MESSAGES.consumedBon(user, bon, coverage_when_consuming, status, number_of_liter_to_consume)
     );
-
+      
+    const getUser = await context.prisma.user({ id: user });
     await sendSms(
       getUser.phone,
       MESSAGES.consumedBon(user, bon, coverage_when_consuming, status, number_of_liter_to_consume)
