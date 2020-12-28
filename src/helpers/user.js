6ed8eab13c040a4ee98cb5b81d95e65533154d57
Neword <newordn@gmail.com>
2020-12-28 12:54:36 +0100

    updated getUserByHoldAndRole helper

--- src/helpers/user.js
+++ src/helpers/user.js
@@ -12,7 +12,11 @@ function getUserId(context) {
 }
 const getUserByHoldAndRole = async (context, hold, role)=>{
 const users = await context.prisma.hold({ id: hold }).users()
-    return users.filter(user=>user.role===role)[0]
+const usersByRole = users.filter(user=>user.role===role)
+if(usersByRole.length>0){
+return usersByRole[0]
+}
+return null
 }
 
 module.exports = {
