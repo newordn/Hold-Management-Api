71f73d8fbae463f5dd312e841b260e43c6a10e80
Neword <newordn@gmail.com>
2020-12-29 06:03:28 +0100

    Refratoring, added transfertBon mutation

--- src/consts/messages.js
+++ src/consts/messages.js
@@ -1,10 +1,22 @@
-const suffixe = "\nMobile: https://play.google.com/store/apps/details?id=com.holdmanagementmobile\nWeb: https://bir-hold-management.com"
-const prefixe = "Bir Hold Management Mobile\n"
+const suffixe =
+  "\nMobile: https://play.google.com/store/apps/details?id=com.holdmanagementmobile\nWeb: https://bir-hold-management.com";
+const prefixe = "Bir Hold Management Mobile\n";
+const createMessage = (message) => `${prefixe}${message}${suffixe}`;
 const MESSAGES = {
-  signUp: (name) => `${prefixe}Inscription de l'utilisateur immatriculé ${name}, veuillez vous connectez avec vos identifiants sur la plateforme${suffixe}`,
-  signIn: (name) => `${prefixe}Connexion de l'utilisateur immatriculé ${name}\nBir Hold Management`,
-  hold: (name) => `${prefixe}Création de la soute de ${name}\nBir Hold Management`,
-  holdLevel: (name,type,quantity)=> `${prefixe}Niveau critique des cuves, nous vous informons que la soute de ${name}, a atteint un seuil critique de ${quantity} litres en contenance ${type}${suffixe}`,
+  transfertBon: (from, to, number_of_liter, fuel_type, motif) =>
+    createMessage(
+      `Transfert de l'utilisateur ${from} de ${number_of_liter} litres a l'utilisateur ${to} de ${super_type} pour ${motif}`
+    ),
+  signUp: (name) =>
+    createMessage(
+      "Inscription de l'utilisateur immatriculé ${name}, veuillez vous connectez avec vos identifiants sur la plateforme"
+    ),
+  signIn: (name) => createMessage(`Connexion de l'utilisateur immatriculé ${name}`),
+  hold: (name) => createMessage(`Création de la soute de ${name}`),
+  holdLevel: (name, type, quantity) =>
+    createMessage(
+      `Niveau critique des cuves, nous vous informons que la soute de ${name}, a atteint un seuil critique de ${quantity} litres en contenance ${type}`
+    ),
   dotateEmetteur: (
     responsableSoute,
     start_date,
@@ -14,9 +26,13 @@ const MESSAGES = {
     number_of_liter_super,
     number_of_liter_gazoil
   ) =>
-    `${prefixe}Dotation de l'utilisateur immatriculé ${responsableSoute} a l'utilisateur immatriculé ${user} avec les quantités super: ${number_of_liter_super} et gazoil: ${number_of_liter_gazoil} pour ${motif} valable du ${start_date} au ${end_date}${suffixe}`,
-  updateUsersHoldRole: (user, hold, role,name) =>
-    `${prefixe}Mise a jour de l'utilisateur immatriculé ${user}, assignation a la soute de ${name} immatriculée ${hold} avec pour role ${role}${suffixe}`,
+    createMessage(
+      `Dotation de l'utilisateur immatriculé ${responsableSoute} a l'utilisateur immatriculé ${user} avec les quantités super: ${number_of_liter_super} et gazoil: ${number_of_liter_gazoil} pour ${motif} valable du ${start_date} au ${end_date}`
+    ),
+  updateUsersHoldRole: (user, hold, role, name) =>
+    createMessage(
+      `Mise a jour de l'utilisateur immatriculé ${user}, assignation a la soute de ${name} immatriculée ${hold} avec pour role ${role}`
+    ),
   dotateHold: (
     start_date,
     end_date,
@@ -27,11 +43,17 @@ const MESSAGES = {
     reserve_super_quantity,
     reserve_gazoil_quantity
   ) =>
-    `${prefixe}Dotation de l'utilisateur immatriculé ${user} a la soute immatriculée ${hold} avec les quantités super: ${super_quantity}, gasoil: ${gazoil_quantity}, reserve_super: ${reserve_super_quantity} , reserve_gasoil: ${reserve_gazoil_quantity} pour une période allant de ${start_date} a ${end_date}${suffixe}`,
+    createMessage(
+      `Dotation de l'utilisateur immatriculé ${user} a la soute immatriculée ${hold} avec les quantités super: ${super_quantity}, gasoil: ${gazoil_quantity}, reserve_super: ${reserve_super_quantity} , reserve_gasoil: ${reserve_gazoil_quantity} pour une période allant de ${start_date} a ${end_date}`
+    ),
   resetPassword: (matricule, password, newPassword) =>
-    `${prefixe}Rénitialisation du mot de passe de l'utilisateur immatriculé ${matricule} dont l'ancien mot de passe est proche de ceci ${password} et le nouveau ${newPassword} ${suffixe}`,
+    createMessage(
+      `Rénitialisation du mot de passe de l'utilisateur immatriculé ${matricule} dont l'ancien mot de passe est proche de ceci ${password} et le nouveau ${newPassword} `
+    ),
   car: (user, hold, marque, capacity, type, immatriculation, kilometrage) =>
-    `${prefixe}L'utilisateur immatriculé ${user} effectue la création du véhicule de marque ${marque} immatriculé ${immatriculation} d'une capacité de ${capacity} consommant du ${type} qui a kilometrage de ${kilometrage} qui réside a la soute immatriculé ${hold}${suffixe}`,
+    createMessage(
+      `L'utilisateur immatriculé ${user} effectue la création du véhicule de marque ${marque} immatriculé ${immatriculation} d'une capacité de ${capacity} consommant du ${type} qui a kilometrage de ${kilometrage} qui réside a la soute immatriculé ${hold}`
+    ),
   bon: (
     reserve,
     expiration_date,
@@ -44,12 +66,17 @@ const MESSAGES = {
     car,
     holds,
     driver
-  ) => `${prefixe}L'utilisateur immatriculé ${user} emet le bon pour du ${fuel_type} ${reserve ? "(consommation réserve)": "(consommation ordinaire)"} quittant de ${departure} pour ${destination} de ${initial_number_of_liter} litres 
-  qui expire le ${expiration_date} avec pour motif/service ${reason} conduit par ${driver} a consommé dans les soutes immatriculées ${holds} avec la voiture immatriculé ${car}${suffixe}`,
+  ) =>
+    createMessage(`L'utilisateur immatriculé ${user} emet le bon pour du ${fuel_type} ${
+      reserve ? "(consommation réserve)" : "(consommation ordinaire)"
+    } quittant de ${departure} pour ${destination} de ${initial_number_of_liter} litres 
+  qui expire le ${expiration_date} avec pour motif/service ${reason} conduit par ${driver} a consommé dans les soutes immatriculées ${holds} avec la voiture immatriculé ${car}`),
   consumedBon: (user, bon, coverage_when_consuming, status, number_of_liter_to_consume) =>
-    `${prefixe}Consommation de ${number_of_liter_to_consume} L du bon immatriculé ${bon} par l'utilisateur immatriculé ${user}, kilométrage lors de la consommation: ${coverage_when_consuming}, ${
-      status ? "reussie" : "échec"
-    }${suffixe}`
+    createMessage(
+      `Consommation de ${number_of_liter_to_consume} L du bon immatriculé ${bon} par l'utilisateur immatriculé ${user}, kilométrage lors de la consommation: ${coverage_when_consuming}, ${
+        status ? "reussie" : "échec"
+      }`
+    )
 };
 module.exports = {
   MESSAGES
--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -200,7 +200,6 @@ async function dotateHold(parent, args, context, info) {
       user: { connect: { id: user } }
     });
     let responsableSoute = await getUserByHoldAndRole(context, hold, ROLES.responsableSoute);
-    console.log(responsableSoute, "responsable soute")
     if(responsableSoute)
     sendSms(
       responsableSoute.phone,
@@ -540,6 +539,49 @@ async function dotateEmetteur(parent, args, context, info) {
     throw new Error(e.message);
   }
 }
+const transfertBon = async (parent, args, context, info) => {
+  try{
+    const from = await context.prisma.user({id: args.from})
+    const to = await context.primsa.user({id: args.to})
+    const restant = args.fuel_type === FUEL.super ? from.super - args.number_of_liter : from.gazoil - args.number_of_liter;
+    const more = args.fuel_type === FUEL.super ? to.super + args.number_of_liter : to.gazoil + args.number_of_liter;
+    console.log(MESSAGES.transfertBon(from.phone,to.phone,args.number_of_liter, args.fuel_type,args.motif))
+    if(restant<0)
+    throw new Error("Vous n'avez pas assez de bons(litres)")
+    await context.prisma.updateUser({
+      data:{
+        super: args.fuel_type=== FUEL.super ? restant: from.super,
+        gazoil: args.fuel_type=== FUEL.gazoil ? restant: from.gazoil
+      },
+      where:{
+        id: args.from
+      }
+    })
+    await context.prisma.updateUser({
+      data:{
+        super: args.fuel_type=== FUEL.super ? more: to.super,
+        gazoil: args.fuel_type=== FUEL.gazoil ? more: to.gazoil
+      },
+      where:{
+        id: args.to
+      }
+    })
+    await context.prisma.createLog({
+      action: MESSAGES.transfertBon(from.phone,to.phone,args.number_of_liter, args.fuel_type,args.motif),
+      user: { connect: { id: args.from} }
+    });
+    sendSms(
+      to.phone,
+      MESSAGES.transfertBon(from.phone,to.phone,args.number_of_liter, args.fuel_type,args.motif),
+      args.to,
+      context
+    );
+  }
+  catch (e) {
+    console.log(e);
+    throw new Error(e.message);
+  }
+}
 module.exports = {
   signUp,
   signIn,
@@ -550,5 +592,6 @@ module.exports = {
   car,
   bon,
   consumedBon,
-  dotateEmetteur
+  dotateEmetteur,
+  transfertBon
 };
